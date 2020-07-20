e48824e98d1a3dbb11de1740cdeba9567659837e
BasiaM1 <b.murzynowska@wp.pl>
2020-07-21 00:27:46 +0200

    Add layout to general site

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,32 @@
 'use strict';
 
-function toggleMenu(visible) {
-  document.querySelector('.sidebar').classList.toggle('hidden', visible);
+function toggleMenuSm(visible) {
+  if (window.screen.width < 768) {
+    document.querySelector('.sidebar').classList.toggle('hidden-sidebar', visible);
+    document.querySelector('.top-menu').classList.toggle('hidden-topmenu', visible);
+  }
 }
 
 document.querySelector('.burger').addEventListener('click', function (e) {
   e.preventDefault();
-  toggleMenu();
+  toggleMenuSm();
+});
+
+function toggleMenuLg(visible) {
+  if (window.screen.width >= 768) {
+    document.querySelector('.sidebar').classList.toggle('hidden-sidebar', visible);
+  }
+}
+
+document.querySelector('.burger').addEventListener('click', function (e) {
+  e.preventDefault();
+  toggleMenuLg();
 });
 
 if (window.screen.width < 768) {
-  document.querySelector('.sidebar').classList.add('hidden');
+  document.querySelector('.sidebar').classList.add('hidden-sidebar');
+  document.querySelector('.top-menu').classList.add('hidden-topmenu');
 } else {
-  document.querySelector('.sidebar').classList.remove('hidden');
+  document.querySelector('.sidebar').classList.remove('hidden-sidebar');
+  document.querySelector('.top-menu').classList.remove('hidden-topmenu');
 }
