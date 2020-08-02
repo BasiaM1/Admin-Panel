0f016fc6b7c8a5e249e777dd266da9756fe81cfb
BasiaM1 <b.murzynowska@wp.pl>
2020-08-02 17:45:43 +0200

    Change modal

--- docs/js/script.js
+++ docs/js/script.js
@@ -36,3 +36,42 @@ if (window.screen.width < 768) {
   document.querySelector('.sidebar').classList.remove('hidden-sidebar');
   document.querySelector('.top-menu').classList.remove('hidden-topmenu');
 }
+
+//zamykanie modali
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+//zamknięcie poprzez klinkięcie w x lub w przycisk
+document.querySelectorAll('#overlay .close-modal').forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+//zamknięcie modala po kliknięciu w tło overlaya
+document.querySelector('#overlay').addEventListener('click', function (e) {
+  if (e.target === this) {
+    closeModal();
+  }
+});
+
+//zamknięcie poprzez wcisnięcie esc
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
+//otwieranie modali - zamknięcie wszsytkich modali wewnątrz overlay a nastepnie otwarcie wskazanego modala
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function (modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+document.querySelector('.user-login').addEventListener('click', function (e) {
+  e.preventDefault();
+  openModal('#modal-login');
+});
